192ce9a6f69446b8127555838acb400308424e1e
Kayla Reopelle <kreopelle@gmail.com>
2018-06-28 21:32:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,6 +34,7 @@ class Dog
 
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
   def self.create(attr_hash)
