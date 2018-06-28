dd60d9f6ce7861ff0f2637e9703334dc2398283a
Kayla Reopelle <kreopelle@gmail.com>
2018-06-28 21:52:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,6 +56,7 @@ class Dog
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
+    binding.pry
     self.new_from_db(result)
   end
 
